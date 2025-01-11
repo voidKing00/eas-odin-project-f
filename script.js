af8f114f8b7cacc6ca5cb3afee411b0fdaddacfa
Kenjie Tarasona <148972543+voidKing00@users.noreply.github.com>
2025-01-11 12:56:06 +0800

    Add paint brush effect on the canvas

--- script.js
+++ script.js
@@ -1,28 +1,30 @@
-console.log("test connection");
-
 const sketchpadContainer = document.querySelector("#sketchpad-container");
+
 const baseRowContainer = document.createElement("div");
+baseRowContainer.style.display = "flex";
 
 const basePixel = document.createElement("div");
 basePixel.classList.add("pixel");
-
-baseRowContainer.style.display = "flex";
 
 let gridLength = 16;
 
 for(let i = 0; i < gridLength; i++){
 
-    const clonePixel = basePixel.cloneNode();
-    baseRowContainer.appendChild(clonePixel);
-    
-}
+    const cloneRowContainer = baseRowContainer.cloneNode();
 
-for(let i = 0; i < gridLength; i++){
+    for(let i = 0; i < gridLength; i++){
 
-    const cloneRowContainer = baseRowContainer.cloneNode(true);
+        const clonePixel = basePixel.cloneNode();
+
+        clonePixel.addEventListener("mouseover" , () => clonePixel.style.backgroundColor = "black");
+
+        cloneRowContainer.appendChild(clonePixel);
+        
+    }
+
     sketchpadContainer.appendChild(cloneRowContainer);
 
 }
-// sketchpadContainer.appendChild(baseRowContainer);
-// sketchpadContainer.appendChild(cloneRowContainer);
+
+
 
